af5652cdb172278825918f09164abf9b844ca2e8
Choulak Ahmed <ahmedchoulak2004@gmail.com>
2024-10-18 09:13:35 +0200

    continuation des TD

--- AprentissageC/TD4/Exo5/main.c
+++ AprentissageC/TD4/Exo5/main.c
@@ -1,31 +1,52 @@
 #include <stdio.h>
 
-
-void dessiner_damier(int t,int c )
-{
-    int tourX;   
+void dessiner_damier(int t, int c) {
+        /*int ligne = 0,tourX=1,changmentdeligne =0;
     for(int i = 0 ; i < t*c; i++){
-        if(i % 2 == 0)
-            tourX = 1;
-        else
-            tourX = 0;
-        for(int j=0;j<t;j++){
-            if(tourX){
-                for(int k=0;k<c;k++){
+        if(tourX){
+            for(int j =0;j<t;j++){
+            if(ligne % 2 == 0)
+                for(int k=0;k<c;k++) printf("X");
+            else
+                for(int k=0;k<c;k++) printf(".");
+            ligne ++;
+        }
+        printf("\n");
+        }else{
+            for(int j =0;j<t;j++){
+            if(ligne % 2 == 0)
+                for(int k=0;k<c;k++) printf(".");
+            else
+                for(int k=0;k<c;k++) printf("X");
+            ligne ++;
+        }
+        printf("\n");
+        }
+        if(++changmentdeligne == c){
+            tourX = !tourX;
+            changmentdeligne = 0;
+        }
+        }
+    */
+
+    for (int i = 0; i < t * c; i++) {  // Parcours des lignes du damier
+        for (int j = 0; j < t; j++) {  // Parcours des colonnes du damier
+            // Alternance des motifs : soit "X", soit "."
+            if ((i / c + j) % 2 == 0) {
+                for (int k = 0; k < c; k++) {
                     printf("X");
                 }
-                tourX = !(tourX);
-            }else{
-                 for(int k=0;k<c;k++){
+            } else {
+                for (int k = 0; k < c; k++) {
                     printf(".");
                 }
-                tourX = !(tourX);
             }
         }
         printf("\n");
     }
 }
-int main(){
-    dessiner_damier(4,2);
+
+int main() {
+    dessiner_damier(4, 2);  // Test avec t=4 (nombre de cases), c=2 (taille des cases)
     return 0;
 }
