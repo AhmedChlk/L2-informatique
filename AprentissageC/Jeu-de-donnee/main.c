60bd6b2cbe898b75327dd459ec8706f3c02987aa
Choulak Ahmed <ahmedchoulak2004@gmail.com>
2024-11-02 22:25:38 +0100

    ajout du CC jeu de donnee

--- AprentissageC/Jeu-de-donnee/main.c
+++ AprentissageC/Jeu-de-donnee/main.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
     int age_min;
@@ -29,20 +29,25 @@ donnee_t* cree_donnee(int annee, age_t age, int nbr_admis, float pr_reussite) {
     return d;
 }
 
-// Affichage d'une donnée
-void afficher_donnee(donnee_t d) {
-    printf("-----------------------\n"
-           "  annee : %d \n"
-           "  [age_min ; age_max] : [%d,%d]\n"
-           "  nombre admis : %d\n"
-           "  pr reussite : %f \n "
-           "----------------------\n",
-           d.annee, d.age.age_min, d.age.age_max, d.nbr_admis, d.pr_reussite);
+void reallouer(jeu_donnee_t* jeu_donnee) {
+    int nouvelle_taille = jeu_donnee->taille_max_jeu + 1;
+    donnee_t** nouveau_tableau = realloc(jeu_donnee->tab_donnee, nouvelle_taille * sizeof(donnee_t*));
+    if (nouveau_tableau != NULL) {
+        printf("DEBUG: Réallocation réussie, nouvelle taille : %d\n", nouvelle_taille);
+        jeu_donnee->tab_donnee = nouveau_tableau;
+        jeu_donnee->taille_max_jeu = nouvelle_taille;
+    } else {
+        printf("ERREUR : Échec de la réallocation\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
-void liberer_donnee(donnee_t* d) {
-    free(d);
-    d = NULL;
+void ajouter_donnee(donnee_t* d, jeu_donnee_t* jeu_donnee) {
+    if (jeu_donnee->nombre_element_present == jeu_donnee->taille_max_jeu) {
+        reallouer(jeu_donnee);
+    }
+    jeu_donnee->tab_donnee[jeu_donnee->nombre_element_present] = d;
+    jeu_donnee->nombre_element_present++;
 }
 
 jeu_donnee_t* allouer(int taille_max_jeu) {
@@ -59,19 +64,14 @@ jeu_donnee_t* allouer(int taille_max_jeu) {
     return J;
 }
 
-void reallouer(jeu_donnee_t* jeu_donnee, int nouvelle_taille) {
-    donnee_t** nouveau_tableau = realloc(jeu_donnee->tab_donnee, nouvelle_taille * sizeof(donnee_t*));
-    if (nouveau_tableau != NULL) {
-        jeu_donnee->tab_donnee = nouveau_tableau;
-        jeu_donnee->taille_max_jeu = nouvelle_taille;
-    }
-}
-
-void ajouter_donnee(donnee_t* d, jeu_donnee_t* jeu_donnee) {
-    if (jeu_donnee->nombre_element_present == jeu_donnee->taille_max_jeu) {
-        reallouer(jeu_donnee, jeu_donnee->taille_max_jeu + 1);
-    }
-    jeu_donnee->tab_donnee[jeu_donnee->nombre_element_present++] = d;
+void afficher_donnee(donnee_t d) {
+    printf("-----------------------\n"
+           "  annee : %d \n"
+           "  [age_min ; age_max] : [%d,%d]\n"
+           "  nombre admis : %d\n"
+           "  pr reussite : %f \n"
+           "----------------------\n",
+           d.annee, d.age.age_min, d.age.age_max, d.nbr_admis, d.pr_reussite);
 }
 
 void afficher(jeu_donnee_t jeu_donnee) {
@@ -80,22 +80,38 @@ void afficher(jeu_donnee_t jeu_donnee) {
     }
 }
 
+// Libère le jeu de données
 void liberer(jeu_donnee_t** jeu_donnee) {
     for (int i = 0; i < (*jeu_donnee)->nombre_element_present; i++) {
-        liberer_donnee((*jeu_donnee)->tab_donnee[i]);
+        free((*jeu_donnee)->tab_donnee[i]);
     }
     free((*jeu_donnee)->tab_donnee);
     free(*jeu_donnee);
     *jeu_donnee = NULL;
 }
 
+// Lecture des données à partir d'un fichier
+jeu_donnee_t* lire(const char* fname) {
+    donnee_t* d = NULL;
+    jeu_donnee_t* J = allouer(10);  // Taille initiale à 10 pour éviter des realloc trop fréquents
+    int annee, a_min, a_max, nb_bac;
+    float p_bac;
+    FILE *F = fopen(fname, "r");
+    if(F != NULL) {
+        while(fscanf(F, "%d %d %d %d %f", &annee, &a_min, &a_max, &nb_bac, &p_bac) != EOF) {
+            d = cree_donnee(annee, (age_t){a_min, a_max}, nb_bac, p_bac);
+            ajouter_donnee(d, J);
+        }
+        fclose(F);
+    }
+    return J;
+}
+
 int main() {
-    donnee_t* d = cree_donnee(2024, (age_t){18, 20}, 10, 40.0);
-    jeu_donnee_t* J = allouer(10);
-
-    ajouter_donnee(d, J);
-    afficher(*J);
-
-    liberer(&J);
+    jeu_donnee_t* T = lire("cc1_data.txt");
+    if (T != NULL) {
+        //afficher(*T);
+        liberer(&T);
+    }
     return 0;
 }
