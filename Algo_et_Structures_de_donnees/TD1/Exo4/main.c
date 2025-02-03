fde50b9fe111152379772fcc37a58b77919a8657
Choulak Ahmed <ahmedchoulak2004@gmail.com>
2025-02-03 15:38:18 +0100

    resolution du TD de Algo

--- Algo_et_Structures_de_donnees/TD1/Exo4/main.c
+++ Algo_et_Structures_de_donnees/TD1/Exo4/main.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <arbre_utils.h>
 
+int main(){
+    int tab[7] = {10,20,30,40,50,60,70};
+    arbre_t* A = construire_arbre_parfaitement_equilibre(tab,7);
+    affichage_indentation (*A);
+    
+    return 0;
+
+
+
+
+
+
+}
