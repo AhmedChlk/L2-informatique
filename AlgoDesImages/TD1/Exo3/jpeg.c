50b2f142425e6a1c9b9ae7dadc91e7f7f56d3f09
Choulak Ahmed <ahmedchoulak2004@gmail.com>
2025-03-14 12:03:35 +0100

    adding Decompression JPEG

--- AlgoDesImages/TD1/Exo3/jpeg.c
+++ AlgoDesImages/TD1/Exo3/jpeg.c
@@ -1,95 +1,91 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "jpeg.h"
 
-
-void pgm_extract_blk(pgm_t* inpgm, double bloc[N][N],int i,int j){
-    for(int k=0;k<N;k++){
-        for(int l=0;l<N;l++){
-            bloc[k][l] = (double) inpgm->pixels[i+k][j+l];
+// Extraction d'un bloc 8x8 d'une image PGM
+void pgm_extract_blk(pgm_t* inpgm, double bloc[N][N], int i, int j) {
+    for (int k = 0; k < N; k++) {
+        for (int l = 0; l < N; l++) {
+            bloc[k][l] = (double) inpgm->pixels[i + k][j + l];
         }
     }
 }
 
+// Application de la DCT sur un bloc 8x8
 void pgm_dct(double bloc[N][N]) {
-    double DCT[N][N];  
+    double DCT[N][N];
     double Ci, Cj, somme;
-
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             Ci = (i == 0) ? 1.0 / sqrt(2) : 1.0;
             Cj = (j == 0) ? 1.0 / sqrt(2) : 1.0;
-
             somme = 0.0;
             for (int x = 0; x < N; x++) {
                 for (int y = 0; y < N; y++) {
                     somme += bloc[x][y] * 
-                             cos(((2 * x + 1) * i * M_PI) / (2.0 * N)) * 
+                             cos(((2 * x + 1) * i * M_PI) / (2.0 * N)) *
                              cos(((2 * y + 1) * j * M_PI) / (2.0 * N));
                 }
             }
             DCT[i][j] = (2.0 / N) * Ci * Cj * somme;
         }
     }
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    // Recopie du résultat dans le bloc
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
             bloc[i][j] = DCT[i][j];
-        }
-    }
 }
 
-
-void pgm_quantify(double bloc[N][N], int Q[N][N]){
-    int DCT[N][N];
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            DCT[i][j] = round(bloc[i][j] / Q[i][j]);
-        }
-
-    }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            bloc[i][j] = DCT[i][j];
-        }
-    }
+// Quantification d'un bloc 8x8
+void pgm_quantify(double bloc[N][N], int Q[N][N]) {
+    int temp[N][N];
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            temp[i][j] = round(bloc[i][j] / Q[i][j]);
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            bloc[i][j] = temp[i][j];
 }
 
-void pgm_zigzag(double bloc[N][N], int zigzag[N*N]) {
+// Parcours en zigzag d'un bloc 8x8
+void pgm_zigzag(double bloc[N][N], int zigzag[N * N]) {
     int row = 0, col = 0;
     for (int k = 0; k < N * N; k++) {
-        zigzag[k] = (int)bloc[row][col];
-        if ((row + col) % 2 == 0) { 
-            if (col == N - 1) { 
-                row++; 
-            } else if (row == 0) { 
-                col++;  
-            } else { 
-                row--; col++; 
+        zigzag[k] = (int) bloc[row][col];
+        if ((row + col) % 2 == 0) { // diagonale montante
+            if (col == N - 1)
+                row++;
+            else if (row == 0)
+                col++;
+            else {
+                row--;
+                col++;
             }
-        } else {  
-            if (row == N - 1) { 
-                col++;  
-            } else if (col == 0) { 
-                row++;  
-            } else { 
-                row++; col--;  
+        } else { // diagonale descendante
+            if (row == N - 1)
+                col++;
+            else if (col == 0)
+                row++;
+            else {
+                row++;
+                col--;
             }
         }
     }
 }
 
-void pgm_rle(FILE *fd, int zgzg[N*N]){
+// Compression RLE d'un bloc zigzag
+void pgm_rle(FILE *fd, int zgzg[N * N]) {
     int i = 0;
-    
-    while (i < N*N) {
-        if(zgzg[i] == 0){
+    while (i < N * N) {
+        if (zgzg[i] == 0) {
             int count = 1;
-            while ((i + count < N*N) && (zgzg[i] == zgzg[i + count])) {
+            while ((i + count < N * N) && (zgzg[i] == zgzg[i + count]))
                 count++;
-            }
             if (count >= 2)
-                fprintf(fd, "@$%d$\n", count);
+                fprintf(fd, "@%d\n", count);
             else
                 fprintf(fd, "%d\n", zgzg[i]);
             i += count;
@@ -100,7 +96,8 @@ void pgm_rle(FILE *fd, int zgzg[N*N]){
     }
 }
 
-void pgm_to_jpeg(pgm_t *in_pgm,char *fname){
+// Fonction principale de compression JPEG
+void pgm_to_jpeg(pgm_t *in_pgm, char *fname) {
     int Q[N][N] = {
         {16, 11, 10, 16, 24, 40, 51, 61},
         {12, 12, 14, 19, 26, 58, 60, 55},
@@ -111,114 +108,161 @@ void pgm_to_jpeg(pgm_t *in_pgm,char *fname){
         {49, 64, 78, 87, 103, 121, 120, 101},
         {72, 92, 95, 98, 112, 100, 103, 99}
     };
-    FILE* F = fopen(fname,"w");
-    if(F == NULL){
-        fprintf(stderr,"Erreur lors de l'ouverture du fichier %s\n",fname);
+    FILE *F = fopen(fname, "w");
+    if (F == NULL) {
+        fprintf(stderr, "Erreur lors de l'ouverture du fichier %s\n", fname);
         return;
     }
-    fprintf(F,"%s\n","JPEG");
-    fprintf(F,"%d %d\n",in_pgm->width,in_pgm->height);
-    for(int i=0;i + N<in_pgm->height;i+=N){
-        for(int j=0;j + N<in_pgm->width;j+=N){
+    fprintf(F, "JPEG\n");
+    fprintf(F, "%d %d\n", in_pgm->width, in_pgm->height);
+    for (int i = 0; i + N <= in_pgm->height; i += N) {
+        for (int j = 0; j + N <= in_pgm->width; j += N) {
             double bloc[N][N];
-            pgm_extract_blk(in_pgm,bloc,i,j);
+            pgm_extract_blk(in_pgm, bloc, i, j);
             pgm_dct(bloc);
-            pgm_quantify(bloc,Q);
-            int zigzag[N*N];
-            pgm_zigzag(bloc,zigzag);
-            pgm_rle(F,zigzag);
+            pgm_quantify(bloc, Q);
+            int zigzag[N * N];
+            pgm_zigzag(bloc, zigzag);
+            pgm_rle(F, zigzag);
         }
     }
     fclose(F);
 }
 
-int fsize(char *fname){
-    FILE* F = fopen(fname,"r");
-    if(F == NULL){
-        fprintf(stderr,"Erreur lors de l'ouverture du fichier %s\n",fname);
+// Retourne la taille d'un fichier en octets
+int fsize(char *fname) {
+    FILE *F = fopen(fname, "r");
+    if (F == NULL) {
+        fprintf(stderr, "Erreur lors de l'ouverture du fichier %s\n", fname);
         return -1;
     }
-    fseek(F,0L,SEEK_END);
+    fseek(F, 0L, SEEK_END);
     int size = ftell(F);
     fclose(F);
     return size;
 }
 
-void decode_rle_jpeg(const char* fname, int*** zgzg) {
-    FILE* F = fopen(fname, "r");
+// Fonction inverse du parcours zigzag pour reconstruire un bloc 8x8
+void inverse_zigzag(int zigzag[N * N], double bloc[N][N]) {
+    int coords[N * N][2];
+    int row = 0, col = 0;
+    for (int k = 0; k < N * N; k++) {
+        coords[k][0] = row;
+        coords[k][1] = col;
+        if ((row + col) % 2 == 0) { // diagonale montante
+            if (col == N - 1)
+                row++;
+            else if (row == 0)
+                col++;
+            else {
+                row--;
+                col++;
+            }
+        } else { // diagonale descendante
+            if (row == N - 1)
+                col++;
+            else if (col == 0)
+                row++;
+            else {
+                row++;
+                col--;
+            }
+        }
+    }
+    for (int k = 0; k < N * N; k++) {
+        int r = coords[k][0];
+        int c = coords[k][1];
+        bloc[r][c] = zigzag[k];
+    }
+}
+
+// Application de l'inverse de la DCT (IDCT) sur un bloc 8x8
+void pgm_idct(double bloc[N][N]) {
+    double temp[N][N];
+    for (int x = 0; x < N; x++) {
+        for (int y = 0; y < N; y++) {
+            double sum = 0.0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    double Ci = (i == 0) ? 1.0 / sqrt(2) : 1.0;
+                    double Cj = (j == 0) ? 1.0 / sqrt(2) : 1.0;
+                    sum += Ci * Cj * bloc[i][j] *
+                           cos(((2 * x + 1) * i * M_PI) / (2.0 * N)) *
+                           cos(((2 * y + 1) * j * M_PI) / (2.0 * N));
+                }
+            }
+            temp[x][y] = (2.0 / N) * sum;
+        }
+    }
+    for (int x = 0; x < N; x++)
+        for (int y = 0; y < N; y++)
+            bloc[x][y] = temp[x][y];
+}
+
+// Fonction principale de décompression JPEG
+pgm_t* jpeg_to_pgm(char *fname) {
+    FILE *F = fopen(fname, "r");
     if (F == NULL) {
         fprintf(stderr, "Erreur lors de l'ouverture du fichier %s\n", fname);
-        return;
+        return NULL;
     }
-
-    char buffer[256];
-    if (fscanf(F, "%4s", buffer) != 1) {
-        fprintf(stderr, "Erreur de lecture du fichier %s\n", fname);
+    char header[10];
+    fscanf(F, "%s", header);
+    if (strcmp(header, "JPEG") != 0) {
+        fprintf(stderr, "Format non reconnu\n");
         fclose(F);
-        return;
+        return NULL;
     }
-
-    if (strcmp(buffer, "JPEG") != 0) {
-        fprintf(stderr, "Erreur : le fichier %s n'est pas un fichier JPEG\n", fname);
-        fclose(F);
-        return;
-    }
-
     int width, height;
-    if (fscanf(F, "%d %d", &width, &height) != 2) {
-        fprintf(stderr, "Erreur de lecture des dimensions\n");
-        fclose(F);
-        return;
-    }
-
-    int nbr_blocs = (width * height + (N * N - 1)) / (N * N);
-    (*zgzg) = (int**)malloc(nbr_blocs * sizeof(int*));
-    if (*zgzg == NULL) {
-        fprintf(stderr, "Erreur d'allocation mémoire\n");
-        fclose(F);
-        return;
-    }
-
-    for (int i = 0; i < nbr_blocs; i++) {
-        (*zgzg)[i] = (int*)malloc(N * N * sizeof(int));
-        if ((*zgzg)[i] == NULL) {
-            fprintf(stderr, "Erreur d'allocation mémoire\n");
-            fclose(F);
-            return;
-        }
-        memset((*zgzg)[i], 0, N * N * sizeof(int));
-    }
-
-    for (int i = 0; i < nbr_blocs; i++) {
-        printf("DEBUG i=%d\n", i);
-        int j = 0;
-
-        while (j < N * N) {
-            char val[10];
-            if (fscanf(F, "%s", val) != 1) {
-                fprintf(stderr, "Erreur de lecture des valeurs\n");
-                fclose(F);
-                return;
+    fscanf(F, "%d %d", &width, &height);
+    pgm_t *img = pgm_alloc(height, width, 255);
+    int Q[8][8] = {
+        {16, 11, 10, 16, 24, 40, 51, 61},
+        {12, 12, 14, 19, 26, 58, 60, 55},
+        {14, 13, 16, 24, 40, 57, 69, 56},
+        {14, 17, 22, 29, 51, 87, 80, 62},
+        {18, 22, 37, 56, 68, 109, 103, 77},
+        {24, 35, 55, 64, 81, 104, 113, 92},
+        {49, 64, 78, 87, 103, 121, 120, 101},
+        {72, 92, 95, 98, 112, 100, 103, 99}
+    };
+    // Traitement par blocs
+    for (int i = 0; i < height; i += N) {
+        for (int j = 0; j < width; j += N) {
+            int zigzag[N * N];
+            int index = 0;
+            // Décodage RLE pour obtenir 64 coefficients
+            while (index < N * N) {
+                char token[20];
+                if (fscanf(F, "%s", token) != 1)
+                    break;
+                if (token[0] == '@') {
+                    int count = atoi(token + 1);
+                    for (int k = 0; k < count && index < N * N; k++)
+                        zigzag[index++] = 0;
+                } else {
+                    zigzag[index++] = atoi(token);
+                }
             }
-
-            if (val[0] == '@') {
-                int count = 0;
-                if (sscanf(val, "@$%d$", &count) != 1) {
-                    fprintf(stderr, "Erreur de lecture du RLE\n");
-                    fclose(F);
-                    return;
+            double bloc[N][N];
+            inverse_zigzag(zigzag, bloc);
+            // Déquantification
+            for (int x = 0; x < N; x++)
+                for (int y = 0; y < N; y++)
+                    bloc[x][y] = bloc[x][y] * Q[x][y];
+            // Application de l'IDCT
+            pgm_idct(bloc);
+            // Placement du bloc dans l'image
+            for (int x = 0; x < N; x++) {
+                for (int y = 0; y < N; y++) {
+                    int pixel = round(bloc[x][y]);
+                    if (pixel < 0) pixel = 0;
+                    if (pixel > 255) pixel = 255;
+                    img->pixels[i + x][j + y] = (unsigned char) pixel;
                 }
-
-                for (int k = 0; k < count && j < N * N; k++) {
-                    (*zgzg)[i][j] = 0;
-                    j++;
-                }
-            } else {
-                (*zgzg)[i][j] = atoi(val);
-                j++;
             }
         }
     }
-
     fclose(F);
+    return img;
 }
--- AlgoDesImages/TD1/Exo3/jpeg.h
+++ AlgoDesImages/TD1/Exo3/jpeg.h
@@ -1,18 +1,23 @@
 #ifndef JPEG_H
 #define JPEG_H
+
 #include <stdio.h>
 #include "pgm.h"
+
 #define N 8
 
-// JPEG Compression
-void pgm_extract_blk(pgm_t* inpgm, double blk[8][8], int i, int j);
+// Fonctions de compression JPEG
+void pgm_extract_blk(pgm_t* inpgm, double blk[N][N], int i, int j);
 void pgm_dct(double bloc[N][N]);
-void pgm_quantify(double bloc[8][8], int Q[8][8]);
+void pgm_quantify(double bloc[N][N], int Q[N][N]);
 void pgm_zigzag(double bloc[N][N], int zigzag[N*N]);
-void pgm_rle(FILE *fd, int zgzg[64]);
-void pgm_to_jpeg(pgm_t *in_pgm,char *fname);
-int fsize(char* filename);
-// JPEG Decompression
-void decode_rle_jpeg(const char* fname,int*** zgzg);
+void pgm_rle(FILE *fd, int zgzg[N*N]);
+void pgm_to_jpeg(pgm_t *in_pgm, char *fname);
+int fsize(char *filename);
+
+// Fonctions de décompression JPEG
+void inverse_zigzag(int zigzag[N*N], double bloc[N][N]);
+void pgm_idct(double bloc[N][N]);
+pgm_t* jpeg_to_pgm(char *fname);
 
 #endif
