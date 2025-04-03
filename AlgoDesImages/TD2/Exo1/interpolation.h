fe806fba1c043ea792e8dd5d52d81c7c5f4de796
Choulak Ahmed <ahmedchoulak2004@gmail.com>
2025-04-03 12:00:08 +0200

    ajout Algo

--- AlgoDesImages/TD2/Exo1/interpolation.h
+++ AlgoDesImages/TD2/Exo1/interpolation.h
@@ -1,3 +1,20 @@
-#define N 1 // Interpolation d'ordre 1
+// interpolation.h
+#ifndef INTERPOLATION_H
+#define INTERPOLATION_H
 
-double (*B[4])(double) = {B0, B1, B2, B3};
+// ordre d'interpolation 
+#define N 1 
+
+// Prototypes des fonctions d’interpolation
+double B0(double x);
+double B1(double x);
+double B2(double x);
+double B3(double x);
+
+// Déclaration externe du tableau de pointeurs sur fonctions
+extern double (*B[4])(double);
+
+// Autres déclarations
+// interpolation_pgm(pgm_t *image, double x, double y);
+
+#endif
