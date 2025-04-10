5d047fc952b5699bcd438afd561163bdb1df659b
Choulak Ahmed <ahmedchoulak2004@gmail.com>
2025-04-10 11:22:05 +0200

    restructuration et organisation du repo

--- AprentissageC/usr/include/myutils.h
+++
@@ -1,7 +0,0 @@
-#ifndef MYUTILS_H
-#define MYUTILS_H 1
-
-void lire_entier(int* var, char* chaine, char* chaine_erreur);
-void lire_element_entier_tab(int* element ,char* chaine , int indice , char* chaine_erreur);
-
-#endif
